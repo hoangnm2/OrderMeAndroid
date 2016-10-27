00a67f9f4124fd5c8c9219a9d30f77b774da3153
Hoang Nguyen <Hoang.NguyenMinh1@harveynash.vn>
2016-10-27 19:16:28 +0700

    Init basic authenticate

--- app/src/main/java/com/android/orderme/LoginActivity.java
+++ app/src/main/java/com/android/orderme/LoginActivity.java
@@ -1,44 +1,26 @@
 package com.android.orderme;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.client.OrderMeAPIWrapper;
+import com.android.client.OrderMeRestClient;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
-
-import static android.Manifest.permission.READ_CONTACTS;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * A login screen that offers login via email/password.
@@ -80,6 +62,8 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        mEmailView.setText("minhhoang@gmail.com");
+        mPasswordView.setText("12345678");
     }
 
     /**
@@ -130,16 +114,25 @@ public class LoginActivity extends AppCompatActivity {
             } else {
                 errorLog.setVisibility(View.VISIBLE);
                 errorLog.setText("Email and password is incorrect.");
-                mEmailSignInButton.setError("Email and password is incorrect.");
-                Toast.makeText(LoginActivity.this, "Email and password is incorrect.", Toast.LENGTH_LONG);
             }
         }
     }
 
     private boolean authentication(String email, String password) {
         //TODO: hardcode
-        /*OrderMeAPIWrapper wrapper = new OrderMeAPIWrapper();
-        wrapper.login();*/
+        Ion.with(getApplicationContext())
+                .load("http://192.168.78.1:8081/OrderMeAPI/rest/login")
+                .setBodyParameter("identifier", "foo")
+                .asString().setCallback(new FutureCallback<String>() {
+            @Override
+            public void onCompleted(Exception e, String result) {
+                try {
+                    JSONObject json = new JSONObject(result);
+                } catch (JSONException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
         if ("minhhoang@gmail.com".equals(email) && "12345678".equals(password)) {
             return true;
         }
@@ -155,41 +148,5 @@ public class LoginActivity extends AppCompatActivity {
         //TODO: Replace this with your own logic
         return password.length() > 4;
     }
-
-    /**
-     * Shows the progress UI and hides the login form.
-     */
-    /*@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-    }*/
 }
 
